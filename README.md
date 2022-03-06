896da0e8c53b81a8c691ce6bdd042ea514660811
Kieran Williams <zombiefruit2@gmail.com>
2022-03-06 16:57:38 +0200

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# vite-monorepo
+
+A monorepo using:
+
+- Vite / ESBuild
+- NPM 7 Workspaces
+- Typescript and [Typescript Project References](https://www.typescriptlang.org/docs/handbook/project-references.html)
+- React component library
+- Storybook with [Storybook Composition](https://storybook.js.org/docs/react/workflows/storybook-composition)
+- VS Code tasks to `start` and run `storybook` for both app and library via integrated terminal
---
+++ packages/app/src/lib/websocket.ts
@@ -0,0 +1,35 @@
+import type { WebsocketMessage } from '@svelte-monorepo/server/dist/universal/index';
+
+export async function connectToServer() {
+  const ws = new WebSocket('ws://localhost:8080/ws');
+  return new Promise<WebSocket>((resolve) => {
+    const timer = setInterval(() => {
+      if (ws.readyState === 1) {
+        clearInterval(timer);
+        resolve(ws);
+      }
+    }, 10);
+  });
+}
+
+export function getOrCreateCursorFor(messageBody: WebsocketMessage): HTMLElement {
+  const sender = messageBody.sender;
+  const existing = document.querySelector(`[data-sender='${sender}']`) as HTMLElement;
+
+  if (existing) {
+    return existing;
+  }
+
+  const template = document.getElementById('cursor') as HTMLTemplateElement;
+  const cursor = template?.content.firstElementChild?.cloneNode(true) as HTMLElement;
+  const svgPath = cursor?.getElementsByTagName('path')[0];
+
+  if (sender) {
+    cursor.setAttribute('data-sender', sender);
+  }
+
+  svgPath.setAttribute('fill', `hsl(${messageBody.color}, 50%, 50%)`);
+  document.body.appendChild(cursor);
+
+  return cursor;
+}
